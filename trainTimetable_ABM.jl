10269be73553e096c567b109cbf71de0640c0d55
PPetermeier <72546685+PPetermeier@users.noreply.github.com>
2022-03-05 20:47:24 +0100

    beschreibung
    die überlegungen vom gespräch reinkommentiert

--- trainTimetable_ABM.jl
+++ trainTimetable_ABM.jl
@@ -4,6 +4,11 @@ using Agents
     isTrain::Bool
 end
 
+#Struktur Konzept:
+#Knoten = Bahnhöfe mit ID & Kapazität
+#Strecken =  Kanten der Knoten mit: ID, Knoten 1&2, Kapazität, Länge (nicht mehr sicher ob das errechnet werden muss oder nicht)
+#Agenttyp 1 = Zug mit ID, Kapazität, Geschwindigkeit, Startposition, boardable(boolean), Array mit schedule, der sich automatisch bei steps appended
+#Agenttyp 2 = Agent mit ID, Start, Ziel, Zielzeit, Größe der Gruppe, Array mit schedule mit den möglichen Einträgen add train für zusteigen, detrain für verlassen
 mutable struct Mover <: AbstractAgent
     id::Int
     pos::Tuple{Int,Int,Float64}
