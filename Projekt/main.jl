b1bb0407708f87d66628f66814a77d5232034c98
PPetermeier <72546685+PPetermeier@users.noreply.github.com>
2022-03-09 20:23:50 +0100

    visualisierung fehlt
    todo: visualisierung nimmt den griddaten nicht als input, vgl mit quellcode morgen.

--- Projekt/main.jl
+++ Projekt/main.jl
@@ -1,12 +1,14 @@
 using Agents, Plots
 
 # Agentendefinition mit Werten: Need = Energiebedarf / Zyklus, extraction = maximal extrahierte Energie / Zyklus, reserve = maximale Reserve, threshhold = Wert bis zur Duplizierung, restliche reserve wird geteilt, 
-mutable struct BasicConsumer<: GridAgent{2}
+mutable struct BasicConsumer<: AbstractAgent
+    id:: Int
+    pos::Dims{2}
     age:: Int
     max_age::Int
     need::Int
     extraction::Int
-    reserve::Int
+    reserve::Float16
     max_reserve::Int
     threshhold::Int
 end
@@ -40,8 +42,8 @@ function sugar_caps(dims, sugar_peaks, max_sugar, dia = 4)
     return sugar_capacities
 end
 
-
 # Unser Modell braucht keine growth-rate und weniger Agenenten, weil diese sich reproduzieren sollen. 
+
 function altered_sugarscape(; dims = (50, 50),
                                 sugar_peaks = ((5, 15), (45, 35), (25, 25)),
                                 N = 50,                               
@@ -53,14 +55,12 @@ function altered_sugarscape(; dims = (50, 50),
                                 max_sugar = 4
 )
     sugar_capacities = sugar_caps(dims, sugar_peaks, max_sugar, 6)
-    sugar_values = deepcopy(sugar_capacities)
     space = GridSpace(dims)
     properties = Dict(
         :N => N,
         :need_distribution => need_distribution ,
         :extraction_distribution => extraction_distribution,
         :max_age_distribution => max_age_distribution,
-        :sugar_values => sugar_values,
         :sugar_capacities => sugar_capacities,
     )
     model = AgentBasedModel(
@@ -86,19 +86,35 @@ end
 
 model = altered_sugarscape()
 
-heatmap(model.sugar_capacities)
 
+#---------------------- Ab hier experimental
+
+# Addiere den begrenzenden Extraktionswert für den Agenten und subtrahiere den Need
 function harvest!(agent, model)
-    agent.reserve+=maximum(agent.extraction, sugar_values[agent.pos])-agent.need
-
+    placesugarvalue=model.sugar_capacities[agent.pos[1],agent.pos[2]] 
+    
+    if agent.extraction > placesugarvalue
+        agent.reserve += ((model.sugar_capacities[agent.pos[1],agent.pos[2]]) - agent.need)
+    else 
+        agent.reserve += (agent.extraction - agent.need)    
+    end
+    #amount = min(agent.extraction, model.sugar_values[place])
+    #agent.reserve += (amount - agent.need)
 end
 
-function populate(agent,model)
+# Wenn Grenzwert zur Reproduktion überschritten und Platz frei ist, "Zellteilung" auf eine der freien Nachbarorten, Reserve: 1/4 jeweils an Kinder, 1/2 Aufwand für Reproduktion
+function populate!(agent,model)
    if agent.reserve>agent.threshhold
-        empty_space=collect(empty.positions(model))
-        if empty_space ! =nothing
-                resourcepool = 1/4*agent.reserve 
-                agent.reserve*=1/4
+        birthplace = []
+        empty_space = collect(empty_positions(model))
+        for i in nearby_positions(agent.pos, model, 2)
+            if i in empty_space
+                push!(birthplace, i)
+            end
+        end    
+        if length(birthplace) > 0
+                resourcepool = (1/4)*(agent.reserve) 
+                agent.reserve*=resourcepool
                 model.position = rand(empty_space)
                 add_agent_single!(
                 model,
@@ -110,11 +126,55 @@ function populate(agent,model)
                 rand(max_reserve_distribution[1]:max_reserve_distribution[2]),
             20
         )
-            
+        end
+    end        
 end
 
-function survivalcheck(agent)
-    if agent.reserve<0 
+# Letzer Funktionscall sollte seperat am Ende in einer Schleife laufen, weil evtl. vorher geteilt wird
+function survivalcheck!(agent)
+    if agent.reserve <= 0 || agent.age >= agent.max_age
         kill_agent!
     end
 end
+
+function agent_step!(agent, model)
+    harvest!(agent, model)
+    populate!(agent, model)
+    survivalcheck!(agent)
+end
+
+heatmap(model.sugar_capacities)
+
+anim = @animate for i in 1:50
+    step!(model, agent_step!, 1)
+    p1= plotabm(model, as = 3, am = :square, ac = :blue)
+    title!(p1, "Agents\n Step $i")
+    p = plot(p1)
+end
+gif(anim, "sugar.gif", fps = 8)
+
+
+
+#adata, _ = run!(model, agent_step!, 60, adata = [:reserve])
+
+#anim = @animate for i in 1:50
+#    step!(model, agent_step!, 1)
+#    p1 = heatmap(model.sugar_values)
+#    p2 = plotabm(model, as = 3, am = :square)
+#    title!(p1, "Energiepotential")
+#    title!(p2, "Agents \n Step $i" )
+#   p = plot(p1, p2)
+#end 
+#gif(anim, "testgif.gif", fps=8)
+
+#anim2 =  @animate for i in 0:60
+#    histogram(
+#   adata[adata.step .== i, :reserve],
+#   legend = false,
+#color = :black,
+#    nbins = 15,
+
+#title = "step $i",
+#)
+#end
+#gif(anim2, fps = 3)
