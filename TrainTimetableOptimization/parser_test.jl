bc78d83b36b3a05adc81b85e477f207014a5ee19
PPetermeier <72546685+PPetermeier@users.noreply.github.com>
2022-03-12 13:43:29 +0100

    Parser finalisiert & erste Optimierung
    Erster Test mit Optimierungsalgorithmus läuft. Parser ist bei Version 1.0 und muss nur noch verändert werden sollte es sich von Seiten der Simulation ergeben, er ist kompakt programmiert (benötigt jetzt aber Julia 1.7 für das ersetzen mehrere Paare im String in einem Befehl) und ausdokumentiert.

--- TrainTimetableOptimization/parser_test.jl
+++ TrainTimetableOptimization/parser_test.jl
@@ -1,70 +1,49 @@
 using DataFrames #pkg. add Datafrmae
-stationnames = ["ID", "Capacity"]
-
-stations = DataFrame(ID = Int64[], Capacity = Int64[])
-lines = DataFrame(ID = Int64[], Start = Int64[], End = Int64[], Length = Float32[], Capacity = Int64[])
-trains = DataFrame(ID = Int64[], StartingStation = Any[], Speed = Float32[], Capacity = Int64[])
-passengers = DataFrame(ID = Int64[], StartingStation = Int64[], Destination = Int64[], Groupsize = Int64[], Targettime = Int64[])
-
-open("C:\\Users\\Philipp\\Desktop\\Einf.Julia\\InfomaticCup\\test\\simple\\input.txt") do file 
-    phase=0
-    for ln in eachline(file)
-        #println("$(ln)")
-        if length(ln) == 0 || startswith(ln, "#")
-            #println("emptyspacebreak")
-            continue
+function parseinput()
+    #----------------- Die Dataframes initialisieren um dann einfach reinschreiben zu können.       #
+    #----------------- Besonderheit bei Trains: Startation muss Any und genutzte Methode tryparse,  #
+    #----------------- damit NaN eingetragen werden kann für die frei setzbaren Züge                #
+    stations = DataFrame(ID = Int64[], Capacity = Int64[])
+    lines = DataFrame(ID = Int64[], Start = Int64[], End = Int64[], Length = Float32[], Capacity = Int64[])
+    trains = DataFrame(ID = Int64[], StartingStation = Any[], Speed = Float32[], Capacity = Int64[])
+    passengers = DataFrame(ID = Int64[], StartingStation = Int64[], Destination = Int64[], Groupsize = Int64[], Targettime = Int64[])
+    #----------------- Die Schleife für custom parsing. Im Effekt auch ein selbst gebautes SwitchCase, mit continues für Effizienz.                     #
+    #-----------------Phasenvariabel kontrolliert an welcher Stelle des Dokumentes gerade gelesen wird die entsprechenden Anweisungen werden ausgeführt.#
+    open("C:\\Users\\Philipp\\Desktop\\Einf.Julia\\InfomaticCup\\test\\simple\\input.txt") do file 
+        phase=0
+        for ln in eachline(file)
+            if length(ln) == 0 || startswith(ln, "#")
+                continue
+            end
+            if ln == "[Stations]" || ln == "[Lines]" || ln == "[Trains]" || ln == "[Passengers]"
+                phase += 1
+                continue
+            end
+            if phase == 1
+                push!(stations,parse.(Int,split(strip(ln,['S']))))
+                continue
+            end
+            if phase == 2
+                push!(lines, parse.(Float64, split(replace(ln,"L" => "", "S" => "" ))))
+                continue
+            end
+            if phase == 3 
+                push!(trains, tryparse.(Float64, split(replace(ln, "T"=>"", "S"=>""))))
+                continue
+            end
+            if phase == 4
+                push!(passengers, parse.(Int, split(replace(ln, "P"=>"", "S"=>""))))
+                continue
+            end 
         end
-        if ln == "[Stations]" || ln == "[Lines]" || ln == "[Trains]" || ln == "[Passengers]"
-            phase += 1
-            #println("phasechangebreak")
-            continue
-        end
-        if phase == 1
-            content = split(strip(ln, ['S']))
-            content = parse.(Int, content)
-            push!(stations,content)
-            #print(stations)
-            continue
-        end
-        if phase == 2
-            content = replace(ln, 'L'=>"")
-            content = replace(content,'S'=>"")
-            content = split(content)
-            content = parse.(Float64, content)
-            push!(lines,content)
-            #print(lines)
-            continue
-        end
-        if phase == 3 
-            content = replace(ln, 'T'=>"")
-            content = replace(content, 'S'=>"")
-            content = split(content)
-            content = tryparse.(Float64, content)
-            push!(trains,content)
-            #push!(trains,parse.(Float64, split(strip(ln,['T', ,'S']))))
-            continue
-        end
-        if phase == 4
-            content = replace(ln, 'P'=>"")
-            content = replace(content, 'S'=>"")
-            content = split(content)
-            content = parse.(Int, content)
-            push!(passengers,content)
-            #push!(passengers,parse.(Int, split(strip(ln,['P', 'T', 'L','S']))))
-            continue
-        end 
     end
-    #println(phase)
 end
-
-
-println(stations)
-println("|---------------------------------------------------------|")
-println(lines)
-println("|---------------------------------------------------------|")
-println(trains)
-println("|---------------------------------------------------------|")
-println(passengers)
-
-
-#TODO: Prints entfernen, Datenpipelines in einfache Funktionscalls umwandeln von Einzelschritten, relative Pfadangabe ans Laufen bekommen
+    return(stations, lines, trains, passengers)
+    #--------------------------------------------------------------------#   
+    println(stations)
+    println("|---------------------------------------------------------|")
+    println(lines)
+    println("|---------------------------------------------------------|")
+    println(trains)
+    println("|---------------------------------------------------------|")
+    println(passengers)
