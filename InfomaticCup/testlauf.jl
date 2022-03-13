85155879379430a9f18fe716f76653b4aef978ff
TSdel <98597122+TSdel@users.noreply.github.com>
2022-03-13 22:26:35 +0100

    changed testfile

--- InfomaticCup/testlauf.jl
+++ InfomaticCup/testlauf.jl
@@ -6,7 +6,10 @@
 include("..//TrainTimetableOptimization//trainTimetable_ABM.jl")
 
 # Now we write the test Project code
-function main()
+test1()
+test2()
+
+function test1()
     println()
     println("=========================================")
     m1 = initialize("InfomaticCup//test//simple//input.txt")
@@ -14,21 +17,26 @@ function main()
     for agent in allagents(m1)
         println(nextDestination!(agent, m1))
         end
+end
+
+function test2()
+    println()
+    println("=========================================")
+    m1 = initialize("InfomaticCup//test//simple//input.txt")
+
+    println(m1.stations)
+    println(m1.lines)
+    println(string("number of agents: ",nagents(m1)))
+    println(m1.agents)
+    println()
+    for timeunit in 1:5
+        for agent in allagents(m1)
+            agent_step!(agent, m1)
+        end
+        println()
+        println(string("End of Timeunit: ", timeunit))
+        println(string("modelagents: ",m1.agents))
     end
-#     println(m1.stations)
-#     println(m1.lines)
-#     println(string("number of agents: ",nagents(m1)))
-#     println(m1.agents)
-#     println()
-#     optimizeTrains!(m1)
-#     for timeunit in 1:3
-#         for agent in allagents(m1)
-#             agent_step!(agent, m1)
-#         end
-#         println()
-#         println(string("End of Timeunit: ", timeunit))
-#         println(string("modelagents: ",m1.agents))
-#     end
-
-main()
-
+    println(m1.stations)
+    println(m1.lines)
+end
